efae97768631c2244e7f21eb195aa2889f451fdd
aisbakan <aisbakan@assia-inc.com>
2021-12-14 16:15:29 +0300

    improve runner:
    - add external input file option
    - add compute all option
    - print elapsed time

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ Advent of Code 2021
 ## Usage
 
 ```
-    aoc2021 -d <day> -p <part>
+aoc2021.py [-h] [-d DAY] [-p PART] [-i FILE] [-a]
+
+options:
+  -h, --help             show this help message and exit
+  -d DAY, --day DAY
+  -p PART, --part PART
+  -i FILE, --input FILE  external input file
+  -a, --all              compute all
 ```
--- aoc2021.py
+++ aoc2021.py
@@ -2,32 +2,82 @@
 
 import argparse
 import importlib
+import time
+from os import path
 
 
-def read_input(day):
-    with open(day + '/input.txt') as f:
+def input_file(file):
+    if path.exists(file):
+        return file
+    else:
+        raise argparse.ArgumentTypeError()
+
+
+def read_input(file_input):
+    with open(file_input) as f:
         return f.read()
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--day', type=int, required=True)
-    parser.add_argument('-p', '--part', type=int, required=True)
-    args = parser.parse_args()
-
-    package = 'day' + str(args.day)
-    module = 'part' + str(args.part)
+def compute(day, part, input):
+    day = f'day{"0" if day < 10 else ""}{day}'
+    part = f'part{part}'
+    file_input = input if input is not None else f'{day}/input.txt'
 
     try:
-        handler = importlib.import_module(package + '.' + module)
+        handler = importlib.import_module(f'{day}.{part}')
     except ModuleNotFoundError:
-        print(package + ' ' + module + ' is not implemented!')
+        print(day + ' ' + part + ' is not implemented!')
         return -1
     except Exception as e:
         print(e)
         return -1
 
-    print(handler.compute(read_input(package)))
+    input = read_input(file_input)
+    before = time.time()
+    answer = handler.compute(input)
+    after = time.time()
+    elapsed = (after - before) * 1000
+    unit = 'ms'
+    if elapsed < 1:
+        elapsed *= 1000
+        unit = 'μs'
+
+    print(f'{day} {part}')
+    print(f'  answer  :\t {answer}')
+    print(f'  elapsed :\t {int(elapsed)} {unit}')
+
+
+def main():
+    days, part = [], []
+    parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(
+        prog, max_help_position=25, width=120))
+    parser.add_argument('-d', '--day', type=int)
+    parser.add_argument('-p', '--part', type=int)
+    parser.add_argument('-i', '--input', type=input_file,
+                        metavar='FILE', help='external input file')
+    parser.add_argument('-a', '--all', action='store_true', help='compute all')
+    args = parser.parse_args()
+
+    if args.day is None and args.part is None and args.all is False:
+        parser.print_help()
+        return -1
+
+    if args.all:
+        parts = [1, 2]
+
+        for d in range(25):
+            if path.isdir(f'day{"0" if d < 10 else ""}{d}'):
+                days.append(d)
+    elif args.day:
+        days = [args.day]
+        if args.part:
+            parts = [args.part]
+        else:
+            parts = [1, 2]
+
+    for day in days:
+        for part in parts:
+            compute(day, part, args.input)
 
 
 if __name__ == '__main__':
