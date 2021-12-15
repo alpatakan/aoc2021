812c27fba63558ed9e6d00e70f11fe663be839cf
aisbakan <aisbakan@assia-inc.com>
2021-12-15 03:18:05 +0300

    make this pyhton3.8 compatible

--- day02/part1.py
+++ day02/part1.py
@@ -2,12 +2,22 @@ def compute(input):
     position = 0
     depth = 0
     for l in input.splitlines():
-        match l.split():
-            case['forward', n]:
-                position += int(n)
-            case['down', n]:
-                depth += int(n)
-            case['up', n]:
-                depth -= int(n)
+        # for python 3.8 compatibility
+        # match l.split():
+        #     case['forward', n]:
+        #         position += int(n)
+        #     case['down', n]:
+        #         depth += int(n)
+        #     case['up', n]:
+        #         depth -= int(n)
+        lsplit = l.split()
+        direction = lsplit[0]
+        amount = lsplit[1]
+        if direction == 'forward':
+            position += int(amount)
+        elif direction == 'down':
+            depth += int(amount)
+        else:
+            depth -= int(amount)
 
     return position * depth
--- day02/part2.py
+++ day02/part2.py
@@ -3,13 +3,24 @@ def compute(input):
     depth = 0
     aim = 0
     for l in input.splitlines():
-        match l.split():
-            case['forward', n]:
-                position += int(n)
-                depth += aim * int(n)
-            case['down', n]:
-                aim += int(n)
-            case['up', n]:
-                aim -= int(n)
+        # for python 3.8 compatibility
+        # match l.split():
+        #     case['forward', n]:
+        #         position += int(n)
+        #         depth += aim * int(n)
+        #     case['down', n]:
+        #         aim += int(n)
+        #     case['up', n]:
+        #         aim -= int(n)
+        lsplit = l.split()
+        direction = lsplit[0]
+        amount = lsplit[1]
+        if direction == 'forward':
+            position += int(amount)
+            depth += aim * int(amount)
+        elif direction == 'down':
+            aim += int(amount)
+        else:
+            aim -= int(amount)
 
     return position * depth
